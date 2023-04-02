3a767008a60428208bf36ff3196dd0f5690904fd
Swaraj Jadhav <70018714+swaraj54@users.noreply.github.com>
2023-04-02 18:47:10 +0530

    push

---
+++ server.js
@@ -0,0 +1,49 @@
+// Resgister.html  - name, number, email, password  , confirm password, 
+// Varibles - box - text
+// datatypes - 
+// var a = 56789987;
+// console.log(a)
+// console.log(typeof(a))
+// var b = "56789987";
+// console.log(b)
+// console.log(typeof(b))
+// var c = '56789987';
+// console.log(c)
+// console.log(typeof(c))
+// var d = `drtfgyijhyghu`;
+// console.log(d)
+// console.log(typeof(d))
+// var e  = true;
+// console.log(e)
+// console.log(typeof(e))
+// var f  = false;
+// console.log(f)
+// console.log(typeof(f))
+// var g ;
+// console.log(g)
+
+
+
+// var a = 56789987;
+// // console.log(a)
+// var b = 9876543;
+// // console.log(b)
+// var add = a + b;
+// console.log(add);
+// var a = 9876543;
+// var b = '9876543';
+// console.log(a == b) // check data only
+// console.log(a === b) // check data & datatypes
+
+// var a = 234;
+// console.log(a)
+// var b = 234;
+// console.log(b)
+// var c = vghj;
+// console.log(hjkl)
+
+
+var a = 5678;
+var b = 546789
+console.log(a && b)
+console.log(a || b)
