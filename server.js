48f52d44eee573f2c5a44c5ce79dfc4ae167c914
Swaraj Jadhav <70018714+swaraj54@users.noreply.github.com>
2023-04-05 21:34:50 +0530

    updates

--- server.js
+++ server.js
@@ -43,7 +43,85 @@
 // console.log(hjkl)
 
 
-var a = 5678;
-var b = 546789
-console.log(a && b)
-console.log(a || b)
+// var a = 5678;
+// var b = 546789
+// console.log(a && b)
+// console.log(a || b)
+
+
+
+
+
+
+// VARIBLES 
+// VAR - ADD- SUB 
+// LOGICAL OPERATOR && || 
+// DATA TYPES 
+// Number, string, Boolean, undefined
+// typeof()
+// console.log()
+// true, false 
+// Comparion operator 
+//  22 == '22'  -> true
+//  22 === "22"  -> false
+
+
+// console.log(2 == "2") // equal ?
+// console.log(2 != "2") // no equal ?
+// console.log(2 === "2") // equal ?
+// console.log(2 !== "2") // no equal ?
+
+
+// Condition statement 
+// console.log(2 > 1)
+// if else 
+// switch
+// if (2 > 1) {
+//     console.log("awdefsgrdthyfjukilojikhugjyfhtg")
+//     console.log("students")
+// } else {
+//     console.log("krishan ")
+//     console.log("awdiz ")
+// }
+
+// Q. Make a driving licence website, 
+// allow users for dl if there age is above 18 ,
+// if user age is 18 then allow him for ll .
+// if user age is below 18 then dont allow user,
+// if user age is above than 80 then dont allow 
+// var userAge = 10;
+// if (userAge > 80) {
+//     console.log("You are over age for dl.")
+// } else if (userAge == 18) {
+//     console.log("You are allow for Learning L.")
+// } else if (userAge > 18) {
+//     console.log("You are allow for DL.")
+// } else {
+//     console.log("You are under age for dl.")
+// }
+
+
+var namee = "a";
+switch (namee) {
+    case "a":
+        console.log("You are a");
+        break;
+    case "b":
+        console.log("You are b");
+        break;
+    case "c":
+        console.log("You are c");
+        break;
+    default : 
+        console.log("name is different from a,b,c.")
+}
+// Q. Make a driving licence website, 
+// allow users for dl if there age is above 18 ,
+// if user age is 18 then allow him for ll .
+// if user age is below 18 then dont allow user,
+// if user age is above than 80 then dont allow 
+
+
+
+
+
